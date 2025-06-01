9e882d506ef784a873fddb8b3ea619ab8efb0f4f
vanquyt <162158063+vanquyt@users.noreply.github.com>
2025-06-01 19:22:04 +0700

    feat: complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,15 @@ import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
 
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -21,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
